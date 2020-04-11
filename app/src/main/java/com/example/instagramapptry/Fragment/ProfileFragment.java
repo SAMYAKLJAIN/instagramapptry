1570ca4b7f091beb1d0d45c9235c90ced6e4104e
ritvik <ritvikupadhyay2000@gmail.com>
2020-04-11 18:00:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/instagramapptry/Fragment/ProfileFragment.java
@@ -0,0 +1,23 @@
+package com.example.instagramapptry.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.instagramapptry.R;
+
+
+public class ProfileFragment extends Fragment {
+    Button logOutButton;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+}
